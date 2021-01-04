93e28786e72850d3fa8c33c5a1142d4755f5d2e8
Jonas HD <jonas.hermonduc@gmail.com>
2021-01-04 12:25:45 +0100

    trying new repo from git

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+#gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
